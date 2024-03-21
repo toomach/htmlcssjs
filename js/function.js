8f23fc22ebb7d3d5223a7b5d1701e3969c257f59
toomach <yaroshpavlo@gmail.com>
2024-03-21 20:34:18 +0200

    1

---
+++ js/function.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// function foo() {
+//     console.log("turbo");
+// };
+
+// foo(); //виклик функції
+
+// function calcBMI(weight, height) {
+//     let numWeight = weight.replace(",", "."); // replace-приймає 2 параметри ("," - що ми хочемо замінити, "." - на що хочемо замінити)
+//     numWeight = Number.parseFloat(numWeight);
+//     const numHeight = Number.parseFloat(height);
+//     const bmi = numWeight / (numHeight ** 2);
+//     console.log(bmi.toFixed(1)); // toFixed(1) - метод який залишає знаки після крапки
+// };
+
+// const bmi = calcBMI("88,3", "1.75");
+
+//-------------------------------------------------------------------------------------
+
+// function makeTransaction(quantity, pricePerDroid) {
+//     const totalPrice = quantity * pricePerDroid;
+//     console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+// };
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+//--------------------------------------------------------------------------------------
+
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrice = price + deliveryFee;
+//     console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+// };
+
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+//--------------------------------------------------------------------------------------
+
+// function getElementWidth(content, padding, border) {
+//     const boxSizsng = Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
+//     console.log(boxSizsng);
+// };
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+
+
+
