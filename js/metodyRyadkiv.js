19006b244d279e7c5e087b0826fe4c586c6d7351
toomach <yaroshpavlo@gmail.com>
2024-03-24 07:16:48 +0200

    23

---
+++ js/metodyRyadkiv.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// slice
+// toLowerCase / toUpperCase
+// includes
+// startWith / endWith
+// indexOf
+// trim
+
+//------------------------------------------------------------
+
+// const message = "Hello World";
+
+// console.log(message.slice(3, 7)); // повертає рядок з 3 по 7 індекс не включаючи останній (7)
+// console.log(message.slice(3)); // виріже 3 перших індекса і поверне то що залишилося
+// console.log(message.slice(0)); // поверне весь рядок
+// console.log(message.slice()); // поверне весь рядок
+
+//------------------------------------------------------------
+
+// const message = "Hello World";
+
+// console.log(message.toLowerCase()); // повертає рядок у нижньому регістрі
+// console.log(message.toUpperCase()); // повертає рядок у верхньому регістрі
+
+//------------------------------------------------------------
+
+const message = "Hello World";
+
+console.log(message.includes());
+
